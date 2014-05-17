d791b8d57bcbb1ed1f931d122a6b10996f809a8e
Douglas Detoni <douglasdetoni92@gmail.com>
2014-05-16 22:20:07 -0300

    Please, help me.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 #include <time.h>
 
 #include "ChessBoard.cpp"
@@ -11,9 +12,16 @@ int main(int argc, char** argv) {
     srand (time(NULL));
 
     int pos = rand()%8;
-    int board[8] = {pos,0,0,0,0,0,0,0};
-    ChessBoard cb (board);
-    cb.print_board_row(0);
+    int board[] = {pos,0,0,0,0,0,0,0};
+    vector<ChessBoard> list_cb;
+
+    ChessBoard cb (board, 0);
+
+
+    cout << "Teste" << endl;
+    //list_cb = cb.get_neighbors();
+
+    //list_cb.push_back(cb);
 
     return 0;
 }
